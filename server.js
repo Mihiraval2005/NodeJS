b6a4113f8e434712bc3f077fb0d0d9fd60a96bee
MIHIR C RAVAL <145988918+Mihiraval2005@users.noreply.github.com>
2025-02-01 16:46:27 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,18 +1,3 @@
-// creatr server
-
-// const https = require('http');
-
-// const server = https.createServer((req,res)=>{
-//     res.writeHead(200,{'content-type':'text/html'});
-//     res.end(`<h1>sdfsf</h1>`)
-// });
-
-// server.listen(7000,()=>{
-//     console.log("start server")
-// })
-
-
-
 //===================== fs modules create file ,read,renamne,deleta,update much moer ...//==============================
 
 // for create file
@@ -49,7 +34,6 @@
 
 
 
-// Work with File & Folder Paths
 
 // const path = require('path');
 
@@ -75,3 +59,142 @@
 // })
 
 //  Deleting Files
+// const fs = require('fs');
+
+// fs.unlink('My.css',(err)=>{
+//     if(err){
+//         console.log(err)
+//     }
+//     console.log("Delete Success")
+// })
+
+
+// Renaming Files
+// const fs = require('fs');
+
+// fs.rename('My.html','my.js',(err)=>{
+//     if(err){
+//         console.log(err)
+//     }
+//     console.log("rename workerData...")
+// })
+
+
+// Creating  Directories
+
+// const fs = require('fs');
+
+
+// fs.mkdir('LATEAST_NODE',{recursive:true},(err)=>{
+//     if(err){
+//         console.log(err)
+//     }
+
+//     console.log("creates")
+// })
+
+// Remove  Directories
+// const fs = require('fs');
+
+
+// fs.rmdir('LATEAST_NODE',{recursive:true},(err)=>{
+//     if(err){
+//         console.log(err)
+//     }
+
+//     console.log("creates")
+// })
+
+
+
+// https modeuls
+// creatr server
+
+// const https = require('http');
+
+// const server = https.createServer((req, res) => {
+//     res.statusCode = 200;
+//     res.writeHead(200, { 'content-type': 'text/html' });
+//     res.end(`<h1>Welcome Raval...</h1>`)
+//     console.log(req.method,"method");
+//     console.log(req.url,"method");
+// });
+
+// server.listen(4000, () => {
+//     console.log("start server")
+// })
+
+//  Handling Different Routes (URLs)
+
+// const http = require('http');
+
+// const server = http.createServer((req,res)=>{
+//     if(req.url ===  '/'){
+//         res.end("Home")
+//     }
+//     else if(req.url === '/about'){
+//         res.end("about")
+//     }
+//     else{
+//         res.statusCode = 404;
+//         res.end("No Page"   )
+//     }
+// })
+
+// server.listen(4000,()=>{
+//     console.log("server start...")
+// })
+
+
+//  Sending Data in Responses:
+// const http = require('http');
+
+// const server = http.createServer((req,res)=>{
+//    req.statusCode = 200;
+//    res.setHeader('Content-Type','text/html');
+//    res.end("<h1>Hii h1</h1>")
+// })
+
+
+// const server = http.createServer((req, res) => {
+//     req.statusCode = 200;
+//     if (req.url === '/') {
+//         res.setHeader('Content-Type', 'application/json');
+//         const response = {
+//             message: "Helo demo",
+//             status: "Success"
+//         };
+
+//         res.end(JSON.stringify(response));
+//     }
+//     else if (req.url === '/about') {
+//         req.statusCode = 200;
+//         res.setHeader('Content-Type', 'text/html');
+//         res.end("<h1>Hii h1</h1>")
+//     }
+
+// })
+
+// server.listen(4000, () => {
+//     console.log("server start...")
+// })
+
+
+// Path modeules
+
+// const path = require('path');
+
+// const filePath = "/home/user/documents/file.txt";
+
+// console.log("File Name:", path.basename(filePath));
+// console.log("Directory:", path.dirname(filePath));
+// console.log("Extension:", path.extname(filePath));
+// console.log("Absolute Path:", path.resolve(filePath));
+
+// const newPath = path.join(path.dirname(filePath), "newfile.txt");
+// console.log("New File Path:", newPath);
+
+
+
+
+// eventLoop
